b043d20cb95171341a8c69dd82582480571a2ca9
Jon Willis <jon.willis@outlook.com>
2015-11-05 19:26:47 -0600

    Addition of IRepository and SqlRepository classes.

---
+++ Mil.AirForce.Loki.DataAccess/Repositories/SqlRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mil.AirForce.Loki.DataAccess.Interfaces;
+
+namespace Mil.AirForce.Loki.DataAccess.Repositories
+{
+	public class SqlRepository : IRepository
+	{
+		public void Initialize()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
